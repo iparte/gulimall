9c1e5621be6a90b9528a4fad785a0e2f1253b16e
dongwenhan <dongwenhan@frogx.in>
2021-01-07 11:48:43 +0800

    项目初始结构

---
+++ gulimall-member/src/test/java/com/parte/gulimall/gulimallmember/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parte.gulimall.gulimallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
